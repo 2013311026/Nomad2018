a19074ef6479326508187bc94f2adaad446450bd
Dawid Dul <dawid.dul@gmail.com>
2017-12-29 13:50:37 +0100

    Added set functionality requirements to Atom class.

--- support_classes.py
+++ support_classes.py
@@ -1,5 +1,8 @@
 class Atom:
 
+    # Equality epsilon
+    ee = 1e-6
+
     def __init__(self,
                  x=0.0,
                  y=0.0,
@@ -10,6 +13,24 @@ class Atom:
         self.y = y
         self.z = z
         self.t = t
+
+    def __eq__(self, other):
+        if (abs(self.x - other.x) < Atom.ee and
+            abs(self.y - other.y) < Atom.ee and
+            abs(self.z - other.z) < Atom.ee and
+            self.t == other.t):
+
+            return True
+        else:
+            return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+    def __hash__(self):
+        s = str(self.x) + str(self.y) + str(self.z) + str(self.t)
+        return hash(s)
 
 
     def __str__(self):
